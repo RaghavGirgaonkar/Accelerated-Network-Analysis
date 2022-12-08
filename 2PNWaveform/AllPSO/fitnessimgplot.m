83a1524442e82ce6888fab8e86c059e1d06e5720
RaghavGirgaonkar <raghav.girgaonkar@gmail.com>
2022-12-08 11:18:50 -0600

    fixed fitness image plot

--- 2PNWaveform/AllPSO/fitnessimgplot.m
+++ 2PNWaveform/AllPSO/fitnessimgplot.m
@@ -5,9 +5,13 @@ four = readmatrix("1001-2000_1001-2000.txt");
 
 ngridpoints = 2000;
 
+one = one';
 one = one(end:-1:1,1:end);
+two = two';
 two = two(end:-1:1,1:end);
+three = three';
 three = three(end:-1:1,1:end);
+four = four';
 four = four(end:-1:1,1:end);
 
 z = [three, four; one, two];
@@ -17,13 +21,17 @@ z = [three, four; one, two];
 x = linspace(0.1, 70, ngridpoints);
 y = linspace(0.1, 2, ngridpoints);
 
+tau0 = 11.9050;
+tau1p5 = 0.7764;
+
 figure;
-% hold on;
+hold on;
 imagesc(x, y, z);
 axis xy;
 xlabel("\tau_0");
 ylabel("\tau_{1.5}");
 title("Fitness Values: Tau Space")
-% boundary_plot;
+scatter(tau0,tau1p5,70,'red','filled','D','DisplayName','Original Parameters');
+boundary_plot;
 colorbar;
-% hold off;
+hold off;
